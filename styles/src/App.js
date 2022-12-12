df4adc22fe5295620bc3a62fb215adb16ed65089
Dani <jdvillalba2@hotmail.com>
2022-12-12 16:42:37 +0100

    styled ready

--- styles/src/App.js
+++ styles/src/App.js
@@ -7,17 +7,40 @@ const Title = styled.h1`
   margin-botton: 1rem;
 `;
 
-const Wrapper = styled.section`
-font-size:1.5em;
-padding:4em;
-background:DarkOliveGreen;
-text-align:center;
-`
+const Paragraph = styled(Title)`
+  color: YellowGreen;
+`;
+
+const Wrapper = styled.section.attrs((props) => ({
+  id: "header",
+  size: props.size || "1.5em",
+}))`
+  font-size: ${props=>props.size};
+  padding: 4em;
+  background: ${(props) =>
+    props.darkTheme ? "DarkSlateGrey" : "DarkOliveGreen"};
+  text-align: center;
+
+  &:hover h1 {
+    color: SkyBlue;
+  }
+
+  h1 {
+    font-size: 1.5em;
+    text-align: center;
+    color: AliceBlue;
+    margin-botton: 1rem;
+  }
+
+  p {
+    color: YellowGreen;
+  }
+`;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello, World!</Title>
+    <Wrapper darkTheme size="1.7em">
+      <h1>Hello, World!</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste molestias
         ipsam ratione dolore laudantium quas? Id voluptatibus tenetur asperiores
