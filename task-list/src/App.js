d84a64a997ba049ff77da0ea08cf6dcd6f4bea9b
Dani <jdvillalba2@hotmail.com>
2022-12-19 20:36:45 +0100

    TODO complete edit funcionality

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,47 +1,109 @@
-import { useState } from 'react';
-import data from "./mockedDB/tasks.json"
-import TaskList from './components/Tasks/TasksList';
-import TasksInput from './components/Tasks/TasksInput';
-import classes from './App.module.css';
+import { useState, useEffect } from "react";
+import TaskList from "./components/Tasks/TasksList";
+import TasksInput from "./components/Tasks/TasksInput";
+import Hourglass from "./components/UI/Spinners/HourGlass";
+import classes from "./App.module.css";
 
 const App = () => {
-  const [tasks, setNewTask] = useState(data);
+    const [tasks, setNewTask] = useState({});
+    const [pending, setPending] = useState(false);
+    const [error, setError] = useState("");
 
-  const addTaskHandler = enteredText => {
-    setNewTask(prevTasks => {
-      const updatedTasks = [...prevTasks];
-      updatedTasks.unshift({ text: enteredText, id: Math.random().toString(16).substring(2) });
-      return updatedTasks;
-    });
-  };
+    const fetchTasksHandler = async (method = "GET", taskText = "") => {
+        try {
+            setPending(true);
+            setError(null);
+            let response = null;
 
-  const deleteItemHandler = taskId => {
-    setNewTask(prevTasks => {
-      const updatedTasks = prevTasks.filter(task => task.id !== taskId);
-      return updatedTasks;
-    });
-  };
+            if (method === "DELETE") {
+                response = await fetch(
+                    "https://task-list-53682-default-rtdb.europe-west1.firebasedatabase.app/ tasks.json" +
+                        taskText +
+                        ".json",
+                    { method }
+                );
+            } else {
+                response = await fetch(
+                    "https://task-list-53682-default-rtdb.europe-west1.firebasedatabase.app/ tasks.json",
+                    {
+                        method,
+                        headers: {
+                            "Content-Type":
+                                method !== "GET" ? "application/json" : "",
+                        },
+                        body:
+                            method !== "GET"
+                                ? JSON.stringify({ title: taskText })
+                                : null,
+                    }
+                );
+            }
 
-  let content = (
-    <h2 style={{ textAlign: 'center', padding: '1em', border: '1px solid #339900', backgroundColor: '#99cc33' }}>No tasks availables. Add one?</h2>
-  );
+            if (response.ok) {
+                const data = await response.json();
+                if (method === "GET") {
+                    setNewTask(data);
+                } else {
+                    fetchTasksHandler();
+                }
+            }
+        } catch (error) {
+            setError({
+                message: error.message || "Something went wrong",
+            });
+        }
+        setPending(false);
+    };
 
-  if (tasks.length > 0) {
-    content = (
-      <TaskList items={tasks} onDeleteItem={deleteItemHandler} />
+    useEffect(() => {
+        fetchTasksHandler();
+    }, []);
+
+    const addTaskHandler = (enteredText) => {
+        fetchTasksHandler("POST", enteredText);
+    };
+
+    const deleteItemHandler = (taskId) => {
+        fetchTasksHandler("DELETE", taskId);
+    };
+
+    return (
+        <main>
+            <section className={classes["task-form"]}>
+                <TasksInput onAddTask={addTaskHandler} />
+            </section>
+            {pending && <Hourglass />}
+            <section className={classes["tasks-content"]}>
+                {!pending && tasks !== null && error === null && (
+                    <TaskList items={tasks} onDeleteItem={deleteItemHandler} />
+                )}
+                {!pending && tasks === null && !error && (
+                    <h2
+                        style={{
+                            textAlign: "center",
+                            padding: "1em",
+                            border: "1px solid #339900",
+                            backgroundColor: "#99cc33",
+                        }}
+                    >
+                        No tasks availables. Add one?
+                    </h2>
+                )}
+                {!pending && tasks === null && error !== null && (
+                    <h2
+                        style={{
+                            textAlign: "center",
+                            padding: "1em",
+                            border: "1px solid #339900",
+                            backgroundColor: "#99cc33",
+                        }}
+                    >
+                        {error.message}
+                    </h2>
+                )}
+            </section>
+        </main>
     );
-  }
-
-  return (
-    <main>
-      <section className={classes["task-form"]}>
-        <TasksInput onAddTask={addTaskHandler} />
-      </section>
-      <section className={classes["tasks-content"]}>
-        {content}
-      </section>
-    </main>
-  );
 };
 
 export default App;
--- task-list/src/components/Tasks/Item.js
+++ task-list/src/components/Tasks/Item.js
@@ -1,14 +1,46 @@
-import classes from './Item.module.css';
+import classes from "./Item.module.css";
+import { useState, useRef } from "react";
 
-const Item = props => {
+const Item = (props) => {
+  const [itEditing, setIsEditing] = useState(false);
+  const titleRef = useRef(null);
 
   const deleteHandler = () => {
     props.onDelete(props.id);
   };
 
+  const editHandler = () => {
+    titleRef.current.focus();
+    setIsEditing(true);
+  };
+
+  const saveHandler = () => {
+    titleRef.current.blur();
+    props.onEdit(props.id, titleRef.current.textContent);
+    setIsEditing(false);
+  };
+
+  const handlerKeyPress = (e) => {
+    if (e.key === "Enter") {
+      saveHandler();
+    }
+  };
+
   return (
-    <li className={`${classes["task-item"]} ${props.even && classes.even}`} onClick={deleteHandler}>
-      {props.children}
+    <li
+      className={`${classes["task-item"]} ${props.even && classes.even}`}
+      onClick={deleteHandler}
+    >
+      <div
+        onClick={editHandler}
+        onKeyPress={handlerKeyPress}
+        contentEditable={true}
+        suppressContentEditableWarning={true}
+        onBlur={saveHandler}
+        ref={titleRef}
+      >
+        {props.children}
+      </div>
     </li>
   );
 };
